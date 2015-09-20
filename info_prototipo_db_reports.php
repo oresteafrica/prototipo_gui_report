ac34bb1bfa83728e8393152ed4d615c97beec9ec
Oreste <oreste@parlatano.org>
2015-09-20 03:53:43 +0200

    finalization PAs forms PASD-01 not yet included

--- info_prototipo_db_reports.php
+++ info_prototipo_db_reports.php
@@ -124,99 +124,64 @@ $debug if true displays var info
 
 if ( $de == 'A' ) { $debug = true; } else { $debug = false; }
 
-$result = pg_query($con, 'SELECT periodtypeid, startdate, enddate FROM period WHERE periodid = ' . $pe);
-if (!$result) { echo "<p>Error opening organisationunit</p>\n"; exit; }
-
-$r = pg_fetch_assoc($result);
-
-	$startday = substr($r['startdate'], 8, 2);
-	$startmonth_pt = month_pt_from_month_num(substr($r['startdate'], 6, 2));
-	$startyear = substr($r['startdate'], 0, 4);
-
-	$endday = substr($r['enddate'], 8, 2);
-	$endmonth_pt = month_pt_from_month_num(substr($r['enddate'], 6, 2));
-	$endyear = substr($r['enddate'], 0, 4);
-	
-	$start_end_text = 'de ' . $startday . '/' . $startmonth_pt. '/' . 
-		$startyear . ' até ' . $endday . '/' . $endmonth_pt. '/' . $endyear;
-
-
+// ini form name. Variable $fo = datasetid coming from table datavalue via ajax
 $result = pg_query($con, 'SELECT name FROM dataset WHERE datasetid = ' . $fo);
-if (!$result) { echo "<p>Error opening organisationunit</p>\n"; exit; }
+if (!$result) { echo "<p>Error opening dataset</p>\n"; exit; }
 $form_name = pg_fetch_assoc($result)['name'];
+// end form name
 
+// ini form template
+$form_template_file = $form_name . '_' . $le . '.html' ;
+if (! file_exists('tpl/'.$form_template_file)) { echo "<p>template file does not exist</p>"; exit; }
+// end form template
+
+// ini period details. Variable $pe = periodid coming from table datavalue via ajax
+$result = pg_query($con, 'SELECT periodtypeid, startdate, enddate FROM period WHERE periodid = ' . $pe);
+if (!$result) { echo "<p>Error opening period</p>\n"; exit; }
+$r = pg_fetch_assoc($result);
+$startday = substr($r['startdate'], 8, 2);
+$startmonth_pt = month_pt_from_month_num(substr($r['startdate'], 6, 2));
+$startyear = substr($r['startdate'], 0, 4);
+$endday = substr($r['enddate'], 8, 2);
+$endmonth_pt = month_pt_from_month_num(substr($r['enddate'], 6, 2));
+$endyear = substr($r['enddate'], 0, 4);	
+$start_end_text = 'de ' . $startday . '/' . $startmonth_pt. '/' . 
+	$startyear . ' até ' . $endday . '/' . $endmonth_pt. '/' . $endyear;
+// end period details
+
+// ini chosen ou and parent ou. Variable $no = sourceid (ou id) coming from table datavalue via ajax
 $result = pg_query($con, 'SELECT name, parentid FROM organisationunit WHERE organisationunitid = ' . $no);
 if (!$result) { echo "<p>Error opening organisationunit</p>\n"; exit; }
 while ($row = pg_fetch_row($result)) {
 $chosen_ou = $row[0];
 $parentid_chosen_ou = $row[1];
 }
-
 if (!$parentid_chosen_ou) { $parentid_chosen_ou = 0; }
-
 $result = pg_query($con, 'SELECT name FROM organisationunit WHERE organisationunitid = ' . $parentid_chosen_ou);
 if (!$result) { echo '<p>Error opening organisationunit, $parentid_chosen_ou = '.$parentid_chosen_ou.'</p>'; exit; }
 $parentname_chosen_ou = pg_fetch_assoc($result)['name'];
+// end chosen ou and parent ou.
 
-$direct_children = [];
-$result = pg_query($con, 'SELECT organisationunitid, name FROM organisationunit WHERE parentid = ' . $no);
-if (!$result) { echo "<p>Error opening organisationunit</p>\n"; exit; }
-while ($row = pg_fetch_row($result)) {
-array_push($direct_children , ['id' => $row[0], 'name' => $row[1]] );
-}
-
-$form_template_file = $form_name . '_' . $le . '.html' ;
-
-if (! file_exists('tpl/'.$form_template_file)) { echo "<p>template file does not exist</p>"; exit; }
-
-
+// ini list of data elements ids according to variable $no coming from table datavalue via ajax
 $dataelementids = [];
 $result = pg_query($con, 'SELECT dataelementid FROM datasetmembers WHERE datasetid = ' . $fo);
 if (!$result) { echo "<p>Error opening datasetmembers</p>\n"; exit; }
 while ($row = pg_fetch_row($result)) {
 	array_push($dataelementids, $row[0] );
 }
+// end list of data elements ids
 
+// ini list of data elements names where value types = integer
 $fulldataelementnames = [];
-foreach ($dataelementids as $de) {
-	$query = 'SELECT name FROM dataelement WHERE valuetype = \'int\' AND dataelementid = ' . $de ;
+foreach ($dataelementids as $di) {
+	$query = 'SELECT name FROM dataelement WHERE valuetype = \'int\' AND dataelementid = ' . $di ;
 	$result = pg_query($con, $query );
 	if (!$result) { echo "<p>Error opening dataelement</p>\n"; exit; }
 	$resultname = pg_fetch_assoc($result)['name'];
 	if ($resultname) { array_push($fulldataelementnames, $resultname); }
 }
 sort($fulldataelementnames, SORT_STRING);
-
-$result_from_direct_children = [];
-foreach ($direct_children as $dc) {
-	foreach ($dataelementids as $de) {
-		$where = 'sourceid = ' . $dc['id'] . ' AND periodid = ' . $pe . ' AND dataelementid = ' . $de;
-		$query = 'SELECT '.
-			'( SELECT name FROM organisationunit WHERE datavalue.sourceid = organisationunit.organisationunitid )'.
-			' as ou , '.
-			'value, '.
-			'( SELECT name FROM dataelement WHERE dataelement.dataelementid = datavalue.dataelementid )'.
-			' as dataelementname '.
-			'FROM datavalue WHERE ' . $where;
-		$result = pg_query($con, $query );
-		if (!$result) { echo "<p>Error opening datavalue</p>\n"; exit; }
-		$res_all = pg_fetch_assoc($result);
-		if ( $res_all ) { array_push($result_from_direct_children, $res_all); }
-	}
-}
-
-$aggregated_sum = [];
-$row = [];
-foreach ($direct_children as $dc) {
-	foreach ($fulldataelementnames as $dn) {
-		$aggregated_value = 0;
-		foreach ($result_from_direct_children as $rc) {
-			if ( $rc['ou'] == $dc['name'] and $rc['dataelementname'] ==  $dn ) { $aggregated_value += $rc['value']; }
-		}
-		$row[$dn] = $aggregated_value;
-	}
-	$aggregated_sum[$dc['name']] = $row;
-}
+// end list of data elements names where value types = integer
 
 $entidade = '';
 $localidade = '';
@@ -235,9 +200,68 @@ switch ($le) {
 	break;
 //---- Distrital -----------------
     case 3:
+
+		// ini list of children names and ids under chosen ou
+		$direct_children = [];
+		$result = pg_query($con, 'SELECT organisationunitid, name FROM organisationunit WHERE parentid = ' . $no);
+		if (!$result) { echo "<p>Error opening organisationunit</p>\n"; exit; }
+		while ($row = pg_fetch_row($result)) {
+		array_push($direct_children , ['id' => $row[0], 'name' => $row[1]] );
+		}
+		// end list of children names and ids under chosen ou
+	
+		// ini value in datavalues for each child each data element within period id
+		$result_from_direct_children = [];
+		foreach ($direct_children as $dc) {
+			foreach ($dataelementids as $di) {
+				$where = 'sourceid = ' . $dc['id'] . ' AND periodid = ' . $pe . ' AND dataelementid = ' . $di;
+				$query = 'SELECT '.
+					'( SELECT name FROM organisationunit WHERE datavalue.sourceid = organisationunit.organisationunitid )'.
+					' as ou , '.
+					'value, '.
+					'( SELECT name FROM dataelement WHERE dataelement.dataelementid = datavalue.dataelementid )'.
+					' as dataelementname '.
+					'FROM datavalue WHERE ' . $where;
+				$result = pg_query($con, $query );
+				if (!$result) { echo "<p>Error opening datavalue</p>\n"; exit; }
+				$res_all = pg_fetch_assoc($result);
+				if ( $res_all ) { array_push($result_from_direct_children, $res_all); }
+			}
+		}
+		// end value in datavalues for each child each data element within period id
+
+		// ini build PAs table from array $result_from_direct_children
+		$aggregated_sum = [];
+		$row = [];
+		foreach ($direct_children as $dc) {
+			foreach ($fulldataelementnames as $dn) {
+				$aggregated_value = 0;
+				foreach ($result_from_direct_children as $rc) {
+					if ( $rc['ou'] == $dc['name'] and $rc['dataelementname'] ==  $dn ) { $aggregated_value += $rc['value']; }
+				}
+				$row[$dn] = $aggregated_value;
+			}
+			$aggregated_sum[$dc['name']] = $row;
+		}
+		// end build PAs table from array $result_from_direct_children
+		
 		$distrito = $chosen_ou;
 		$provincia = $parentname_chosen_ou;
+
+		if (! $debug) {
+			$template_array = array(
+				'rep_cho'			=> $chosen_ou,
+				'rep_mes' 			=> $mes,
+				'rep_ano' 			=> $ano,
+				'rep_par' 			=> $parentname_chosen_ou,
+				'aggregated_sum'	=> $aggregated_sum
+			);
+			$template = $twig->loadTemplate($form_template_file);
+			echo $template->render($template_array);
+		}
+		
 	break;
+
 //---- Posto Administrativo ------
     case 4:
 		$pa = $chosen_ou;
@@ -248,24 +272,69 @@ switch ($le) {
 		$result = pg_query($con, 'SELECT name FROM organisationunit WHERE organisationunitid = ' . $temp);
 		if (!$result) { echo "<p>Error opening organisationunit</p>\n"; exit; }
 		$provincia = pg_fetch_assoc($result)['name'];
-
-		switch ($form_name) {
-			case 'PROT-02':
-			break;
-			case 'PRESCO-01':
-			break;
-			default:
-				echo '<p style="color:red;font-weight:bold;">incorrect form</p>'; exit;
-		}
 		
+		// ini value in datavalues for chosen PA each data element within period id
+		$result_from_pa = [];
+		foreach ($dataelementids as $di) {
+			$wherepa = 'sourceid = ' . $no . ' AND periodid = ' . $pe . ' AND dataelementid = ' . $di;
+			$querypa = 'SELECT '.
+				'( SELECT name FROM dataelement WHERE dataelement.dataelementid = datavalue.dataelementid )'.
+				' as dataelementname, '.
+				'value '.
+				'FROM datavalue WHERE ' . $wherepa;
+			$result = pg_query($con, $querypa );
+			if (!$result) { echo "<p>Error opening datavalue</p>\n"; exit; }
+			$res_all = pg_fetch_assoc($result);
+			if ( $res_all ) { array_push($result_from_pa, $res_all); }
+		}
+		// end value in datavalues for chosen PA each data element within period id
+
+		$form_value = [];
+		if ($result_from_pa) {
+
+			// ini build form table from array $result_from_pa
+			foreach ($fulldataelementnames as $dn) {
+				$arsearch = array_search($dn,array_column($result_from_pa, 'dataelementname'));
+				if ( $arsearch !== false ) {
+					$form_value[$dn] = $result_from_pa[$arsearch]['value'];
+					// array_push($form_value, [$dn,$result_from_pa[$arsearch]['value']]);
+				} else {
+					$form_value[$dn] = '';
+					//array_push($form_value, [$dn,'']);
+				}
+			}
+			// end build form table from array $result_from_pa
+
+			if (! $debug) {
+				$template_array = array(
+					'rep_cho'			=> $chosen_ou,
+					'rep_mes' 			=> $mes,
+					'rep_ano' 			=> $ano,
+					'rep_par' 			=> $parentname_chosen_ou,
+					'rep_ent' 			=> $entidade,
+					'rep_loc' 			=> $localidade,
+					'rep_ins' 			=> $instituição,
+					'rep_frm'			=> $form_value
+				);
+				$template = $twig->loadTemplate($form_template_file);
+				echo $template->render($template_array);
+			}
+		} else {
+			echo '<p>Não existem dados de acordo com as informações seguintes</p>';
+			echo '<p>ano <b>' . $endyear . '</b></p>';
+			echo '<p>mês <b>' . $endmonth_pt . '</b></p>';
+			echo '<p>Posto Administrativo <b>' . $pa . '</b></p>';
+			echo '<p>Distrito <b>' . $distrito . '</b></p>';
+			echo '<p>Província <b>' . $provincia . '</b></p>';
+			echo '<p>Formulário <b>' . $form_name . '</b></p>';
+		}
+	
 	break;
+
 //--------------------------------
     default:
 
 }
-
-
-
 
 //====================================== ini bebug ===========================================================
 if ($debug) {
@@ -320,49 +389,16 @@ echo $direct_children[0]['name'];
 !Kint::dump( $aggregated_sum[$direct_children[0]['name']] );
 }
 
+echo '<label>last $querypa</label><br /><textarea style="width:80%">' . $querypa .  '</textarea>';
+echo '<br />';
+!Kint::dump( $result_from_pa );
+echo '<br />';
+!Kint::dump( $form_value );
+
 return;
 
 }
 //====================================== end bebug ===========================================================
-
-switch ($le) {
-//---- Nacional -----------------
-	case 1:
-//---- Provincial ----------------
- 	case 2:
-		not_yet();
-	break;
-//---- Distrital -----------------
-    case 3:
-		$template_array = array(
-			'rep_cho'			=> $chosen_ou,
-			'rep_mes' 			=> $mes,
-			'rep_ano' 			=> $ano,
-			'rep_par' 			=> $parentname_chosen_ou,
-			'aggregated_sum'	=> $aggregated_sum
-		);
-		$template = $twig->loadTemplate($form_template_file);
-		echo $template->render($template_array);
-	break;
-//---- Posto Administrativo ------
-    case 4:
-		$template_array = array(
-			'rep_cho'			=> $chosen_ou,
-			'rep_mes' 			=> $mes,
-			'rep_ano' 			=> $ano,
-			'rep_par' 			=> $parentname_chosen_ou,
-			'rep_ent' 			=> $entidade,
-			'rep_loc' 			=> $localidade,
-			'rep_ins' 			=> $instituição,
-			'aggregated_sum'	=> $aggregated_sum
-		);
-		$template = $twig->loadTemplate($form_template_file);
-		echo $template->render($template_array);
-	break;
-//--------------------------------
-    default:
-    	echo '<p style="color:red;font-weight:bold;">incorrect level</p>'; exit;
-}
 
 return;
 }
