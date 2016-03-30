1196eb836009908242846a5aaeb1aba394a8c35b
Oreste <oreste@parlatano.org>
2016-03-30 23:49:58 +0200

    debugging Undefined offset: -1

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ $localhosts = array(
 	'::1'
 );
 
-$f(in_array($_SERVER['REMOTE_ADDR'], $localhosts)){
+if(in_array($_SERVER['REMOTE_ADDR'], $localhosts)) {
 ini_set('display_errors', '1');
 error_reporting(E_ALL | E_STRICT);
 }
